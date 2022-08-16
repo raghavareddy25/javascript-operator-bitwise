47a2fdb8acfc192f183f0a0929fd91e47f4b09c0
raghavareddy <raghavareddy2325@gmail.com>
2022-08-17 00:41:05 +0530

    code added

---
+++ bitwise.js
@@ -0,0 +1,40 @@
+// bitwise AND operator example
+
+let a = 12; 
+let  b = 25; 
+let c = 30;
+
+result = a & b; 
+console.log(result); // 8 
+
+// bitwise NOT operator example
+
+result = ~b;
+
+console.log(result); // -26
+
+// bitwise OR operator example
+
+result = a | b; 
+console.log(result); // 29
+
+// bitwise XOR operator example
+
+result = a ^ b; 
+console.log(result); // 21
+
+// bitwise leftshift
+result = a << b;
+
+// 402653184
+console.log(result);
+
+// bitwise rightshift
+result = a >> b;
+result1 = c >> b;
+
+// 0
+console.log(result); 
+
+// 0
+console.log(result1);
